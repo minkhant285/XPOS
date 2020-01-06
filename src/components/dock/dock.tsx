00b745274b3896390fb7b725bbe045b2504a7c27
minkhant285 <bryyan.mk@gmail.com>
2020-01-06 15:37:01 +0630

    separate dock item

--- src/components/dock/dock.tsx
+++ src/components/dock/dock.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './dockStyle.scss'
-import { IconButton, Grid, Typography } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import HomeIcon from '@material-ui/icons/Home';
 import CategoryIcon from '@material-ui/icons/Category';
 import ProductIcon from '@material-ui/icons/Redeem';
@@ -8,47 +8,54 @@ import LoyaltyIcon from '@material-ui/icons/Loyalty';
 
 
 export const Dock = (props: any) => {
-
+     let [Dock_item, DockActivate] = useState('Dock_item')
     const dockSelect = (param: string) => {
         props.sendToParent(param)
+        DockActivate('Dock_item  active')
+    }
+
+    function renderDockItem(){
+        return (
+            <Grid container direction="column">
+                <Grid item direction="column">
+                    <div className={Dock_item} onClick={() => dockSelect('home')}>
+                        <HomeIcon />
+                        <Typography style={{ fontSize: '10px', fontWeight: 'bold' }}>
+                            Home
+                        </Typography>
+                    </div>
+                </Grid>
+                <Grid item>
+                    <div className={Dock_item} onClick={() => dockSelect('category')}>
+                        <CategoryIcon />
+                        <Typography style={{ fontSize: '10px', fontWeight: 'bold' }}>
+                            Category
+                        </Typography>
+                    </div>
+                </Grid>
+                <Grid item>
+                    <div className='Dock_item' onClick={() => dockSelect('product')}>
+                        <ProductIcon />
+                        <Typography style={{ fontSize: '10px', fontWeight: 'bold' }}>
+                            Product
+                        </Typography>
+                    </div>
+                </Grid>
+                <Grid item>
+                    <div className='Dock_item' onClick={() => dockSelect('sale')}>
+                        <LoyaltyIcon />
+                        <Typography style={{ fontSize: '10px', fontWeight: 'bold' }}>
+                            Sales
+                        </Typography>
+                    </div>
+                </Grid>
+            </Grid>
+        )
     }
 
     return (
         <div className='Dock_Container'>
-            <Grid container direction="column">
-                <Grid item style={{marginBottom:'10px'}}>
-                        <IconButton color="inherit" aria-label="menu" onClick={()=>dockSelect('home')}>
-                            <HomeIcon />
-                        </IconButton>
-                        <Typography style={{fontSize:'10px', fontWeight:'bold'}}>
-                            Home
-                        </Typography>
-                </Grid>
-                <Grid item style={{marginBottom:'10px'}}>
-                        <IconButton color="inherit" aria-label="category" onClick={() => dockSelect('category')}>
-                            <CategoryIcon />
-                        </IconButton>
-                        <Typography style={{fontSize:'10px', fontWeight:'bold'}}>
-                            Category
-                        </Typography>
-                </Grid>
-                <Grid item style={{marginBottom:'10px'}}>
-                        <IconButton color="inherit" aria-label="product" onClick={() => dockSelect('product')}>
-                            <ProductIcon />
-                        </IconButton>
-                        <Typography style={{fontSize:'10px', fontWeight:'bold'}}>
-                            Products
-                        </Typography>
-                </Grid>
-                <Grid item style={{marginBottom:'10px'}}>
-                        <IconButton color="inherit" aria-label="product" onClick={() => dockSelect('sale')}>
-                            <LoyaltyIcon />
-                        </IconButton>
-                        <Typography style={{fontSize:'10px', fontWeight:'bold'}}>
-                            Sales
-                        </Typography>
-                </Grid>
-            </Grid>
+            {renderDockItem()}
         </div>
     )
 }
--- src/components/product/product.tsx
+++ src/components/product/product.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Grid, Card, CardContent, CardMedia, Typography } from '@material-ui/core'
+import { Grid, Card, CardContent, CardMedia, Typography, Toolbar, AppBar } from '@material-ui/core'
 
 import './productStyle.scss'
 import '../../theme/Apptheme.scss';
@@ -10,21 +10,27 @@ export const Product: React.FC = () => {
     return (
         <div className="Common_Description_Container">
             <Grid container direction="row">
+                <AppBar position="relative">
+                    <Toolbar color="primary">
+                        <Typography>
+                            Products
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
                 <Grid item className="Product_Card">
                     <Card>
-                    <CardMedia
-                        component="img"
-                        alt="Contemplative Reptile"
-                        height="140"
-                        width="100"
-                        image={require('../../assets/friedpan.jpeg')}
-                        title="Contemplative Reptile"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="h2">
-                           Lizard
+                        <CardMedia
+                            component="img"
+                            alt="Contemplative Reptile"
+                            height="140"
+                            image={require('../../assets/friedpan.jpeg')}
+                            title="Contemplative Reptile"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                Lizard
                         </Typography>
-                    </CardContent>
+                        </CardContent>
                     </Card>
                 </Grid>
             </Grid>
